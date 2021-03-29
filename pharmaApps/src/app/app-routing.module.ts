9c0b07fade40342c6e8bc350fe20096832bcecb1
Evaristeokito <mbukaokito40@gmail.com>
2021-03-29 16:46:32 +0200

    app config

--- pharmaApps/src/app/app-routing.module.ts
+++ pharmaApps/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductAddComponent } from './composants/product-add/product-add.component';
+import { ProductListComponent } from './composants/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'product-list' , component:ProductListComponent},
+  {path : 'product-add' , component:ProductAddComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pharmaApps/src/app/app.module.ts
+++ pharmaApps/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductAddComponent } from './composants/product-add/product-add.component';
+import { ProductListComponent } from './composants/product-list/product-list.component';
 import { HeaderComponent } from './utils/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductAddComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
